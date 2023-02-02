e8079f5d9bb4630eba0f5650c4826b2dae5c82f3
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-02-02 18:37:00 +0700

    fff

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+      Console.Write("Input number : "); 
+      double input = double.Parse(Console.ReadLine());
+      Console.WriteLine(Cal(input));  
+    }
+    static double Cal(double input)
+    {
+      double ans = 0;
+      if(Math.Pow(input,2) > 4)
+      {
+        ans = 1+Cal(input-(4*Math.Abs(input))/input);
+        return ans;
+      }
+      else 
+      {
+        return ans = 2;
+      }
+    }
+}
