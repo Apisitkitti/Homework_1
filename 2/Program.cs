344a3c64839cc6ce75493ae25ed9993a267cd3bc
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-02-06 17:26:19 +0700

    finish

--- 2/Program.cs
+++ 2/Program.cs
@@ -4,11 +4,35 @@ class Program
     static void Main()
     {
         Console.Write("Input : ");
-        double input1 = double.Parse(Console.ReadLine());
-        double input2 = double.Parse(Console.ReadLine());
-    }
-    static double Circle(double input1 , double input2)
-    {
+        double inputXC = double.Parse(Console.ReadLine());
+        double inputYC = double.Parse(Console.ReadLine());
+        double inputXP = double.Parse(Console.ReadLine());
+        double inputYP = double.Parse(Console.ReadLine());
+        Console.Write("input amount: ");
+        double amount = double.Parse(Console.ReadLine());
+        Circle(inputXC,inputYC,inputXP,inputYP,amount);
+        
         
     }
+    static void Circle(double XC, double YC , double XP, double YP,double amount)
+    {
+       double NewXC = 0;
+       double NewYC = 0;
+       double radian = Math.Atan((YP-YC)/(XP-XC));
+       double distance = Math.Sqrt(Math.Pow(YP-YC,2)+Math.Pow(XP-XC,2));
+       for(int i = 1 ;i<=amount; i++)
+       {
+        NewXC = XC;
+        NewYC = YC;
+         XC = XC+(distance/(2*i))*Math.Cos(radian+(radian*2*i));
+         YC = YC+(distance/(2*i))*Math.Sin(radian+(radian*2*i));
+         XP = NewXC+(distance/(Math.Pow(2,i-1)))*Math.Cos(radian+(radian*2*i));
+         YP = NewYC+(distance/(Math.Pow(2,i-1)))*Math.Sin(radian+(radian*2*i));
+       }
+       Console.WriteLine(XC);
+       Console.WriteLine(YC);
+       Console.WriteLine(XP);
+       Console.WriteLine(YP);
+    }
+    
 }
